842696d582be967ad7db9aa465c59d6689915f65
sanjit singh <ss3448@scarletmail.rutgers.edu>
2024-10-24 16:59:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,35 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve gzipped files with the correct content encoding
+app.get('*.js', (req, res, next) => {
+    if (req.url.endsWith('.gz')) {
+        res.set('Content-Encoding', 'gzip');
+        res.set('Content-Type', 'application/javascript');
+    }
+    next();
+});
+
+app.get('*.data', (req, res, next) => {
+    if (req.url.endsWith('.gz')) {
+        res.set('Content-Encoding', 'gzip');
+        res.set('Content-Type', 'application/octet-stream');
+    }
+    next();
+});
+
+app.get('*.wasm', (req, res, next) => {
+    if (req.url.endsWith('.gz')) {
+        res.set('Content-Encoding', 'gzip');
+        res.set('Content-Type', 'application/wasm');
+    }
+    next();
+});
+
 // Serve static files from the WebGL build directory
 app.use(express.static(path.join(__dirname, 'WebGL-Build')));
 
-// Default route to serve the index.html file
+// Serve index.html by default
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'WebGL-Build', 'index.html'));
 });
